e4e95484e169de3197dfcc10c4dbed63b5c59efb
hylcore-V <hyliver0109@gmail.com>
2024-11-28 01:35:41 -0500

    fix: worklet and node rust

--- macros/src/node.rs
+++ macros/src/node.rs
@@ -2,42 +2,56 @@ use proc_macro2::{Ident as ProcIdent, Span};
 use quote::quote;
 use syn::Ident;
 
-// Generates the node wrapper to bind with JS
+// Generates a wrapper for the Audio Node, binding it to JavaScript
 pub fn node_wrapper(ident: &Ident) -> proc_macro2::TokenStream {
+    // Create a new identifier for the Node struct
     let node_ident = ProcIdent::new(&format!("{ident}Node"), Span::call_site());
 
+    // Generate the TokenStream for the wrapper
     quote! {
-      #[wasm_bindgen(js_name = #ident)]
-      pub struct #node_ident (waw::node::Node<#ident>);
+        // The `#[wasm_bindgen]` attribute binds the Rust struct to a JS object.
+        #[wasm_bindgen(js_name = #ident)]
+        pub struct #node_ident(waw::node::Node<#ident>);
 
-      #[wasm_bindgen(js_class = #ident)]
-      impl #node_ident {
-          pub async fn create(
-            ctx: waw::web_sys::AudioContext,
-            initial_state: Option<<#ident as waw::worklet::AudioModule>::InitialState>
-          ) -> Result<#node_ident, wasm_bindgen::JsValue> {
-              let result = waw::node::Node::<#ident>::create(ctx, initial_state).await?;
-              Ok(#node_ident(result))
-          }
-          pub fn node(&self) -> Result<waw::web_sys::AudioWorkletNode, wasm_bindgen::JsValue> {
-            Ok(self.0.inner.clone())
-          }
+        // The JS class name is bound to the struct via the `js_class` attribute.
+        #[wasm_bindgen(js_class = #ident)]
+        impl #node_ident {
+            // `create` is an asynchronous function that creates a new Node instance.
+            pub async fn create(
+                ctx: waw::web_sys::AudioContext,
+                initial_state: Option<<#ident as waw::worklet::AudioModule>::InitialState>
+            ) -> Result<#node_ident, wasm_bindgen::JsValue> {
+                let result = waw::node::Node::<#ident>::create(ctx, initial_state).await?;
+                Ok(#node_ident(result))
+            }
 
-          pub fn get_param(&self, param: <#ident as waw::worklet::AudioModule>::Param) -> waw::web_sys::AudioParam {
-            self.0.get_param(param)
-          }
+            // Returns the underlying AudioWorkletNode.
+            pub fn node(&self) -> Result<waw::web_sys::AudioWorkletNode, wasm_bindgen::JsValue> {
+                Ok(self.0.inner.clone())
+            }
 
-          pub fn command(&self, message: <#ident as waw::worklet::AudioModule>::Command) {
-              self.0.command(message)
-          }
+            // Get the AudioParam for a specific parameter of the module.
+            pub fn get_param(&self, param: <#ident as waw::worklet::AudioModule>::Param) -> waw::web_sys::AudioParam {
+                self.0.get_param(param)
+            }
 
-          pub fn subscribe(&mut self, callback: waw::utils::callback::Callback<<#ident as waw::worklet::AudioModule>::Event>) {
-              self.0.subscribe(callback.0)
-          }
+            // Sends a command to the module's processor.
+            pub fn command(&self, message: <#ident as waw::worklet::AudioModule>::Command) {
+                self.0.command(message)
+            }
 
-          pub fn destroy(&mut self) {
-            self.0.destroy();
-          }
-      }
+            // Subscribes to events from the module's processor.
+            pub fn subscribe(
+                &mut self,
+                callback: waw::utils::callback::Callback<<#ident as waw::worklet::AudioModule>::Event>
+            ) {
+                self.0.subscribe(callback.0)
+            }
+
+            // Destroys the node, cleaning up resources.
+            pub fn destroy(&mut self) {
+                self.0.destroy();
+            }
+        }
     }
 }
--- macros/src/worklet.rs
+++ macros/src/worklet.rs
@@ -2,62 +2,75 @@ use proc_macro2::{Ident as ProcIdent, Span};
 use quote::quote;
 use syn::Ident;
 
-// Generates the worklet wrapper to bind with JS in the audio thread
+// Generates a wrapper for an Audio Worklet Processor in WebAssembly
 pub fn worklet_wrapper(ident: &Ident) -> proc_macro2::TokenStream {
+    // Create a new identifier for the processor, suffixed with "Processor"
     let worklet_ident = ProcIdent::new(&format!("_{ident}Processor"), Span::call_site());
-
     let worklet_ident_name = worklet_ident.to_string();
 
+    // Generate the final TokenStream to be used in the macro
     quote! {
-      use wasm_bindgen::prelude::*;
+        // Import necessary WebAssembly bindings and modules
+        use wasm_bindgen::prelude::*;
+        
+        // Define the struct wrapping the processor for the given module
+        #[wasm_bindgen]
+        pub struct #worklet_ident(waw::worklet::Processor<#ident>);
 
-      #[wasm_bindgen]
-      pub struct #worklet_ident (waw::worklet::Processor<#ident>);
+        // Implement methods for the Worklet Processor
+        #[wasm_bindgen]
+        impl #worklet_ident {
+            // Constructor to initialize the Worklet Processor
+            #[wasm_bindgen(constructor)]
+            pub fn new(
+                js_processor: waw::web_sys::AudioWorkletProcessor,
+                initial_state: Option<<#ident as waw::worklet::AudioModule>::InitialState>
+            ) -> Self {
+                // Create an emitter and processor for the given module
+                let emitter = waw::worklet::Emitter::<
+                    <#ident as waw::worklet::AudioModule>::Event
+                >::new(js_processor.port().unwrap());
 
-      #[wasm_bindgen]
-      impl #worklet_ident {
-        #[wasm_bindgen(constructor)]
-        pub fn new(
-          js_processor: waw::web_sys::AudioWorkletProcessor,
-          initial_state: Option<<#ident as waw::worklet::AudioModule>::InitialState>
-        ) -> Self {
-          let emitter = waw::worklet::Emitter::<
-            <#ident as waw::worklet::AudioModule>::Event
-          >::new(js_processor.port().unwrap());
-          #worklet_ident (waw::worklet::Processor::new(
-            #ident::create(initial_state, emitter),
-            js_processor
-          ))
+                // Return the initialized Worklet Processor
+                #worklet_ident(waw::worklet::Processor::new(
+                    #ident::create(initial_state, emitter),
+                    js_processor
+                ))
+            }
+
+            // Method to connect the processor
+            pub fn connect(&mut self) {
+                self.0.connect();
+            }
+
+            // Method to process input, output, and parameters
+            pub fn process(
+                &mut self,
+                input: &waw::js_sys::JsValue,
+                output: &waw::js_sys::Array,
+                params: &wasm_bindgen::JsValue
+            ) -> bool {
+                self.0.process(input, output, params)
+            }
+
+            // Retrieve the parameter descriptor for the processor
+            pub fn parameter_descriptor() -> String {
+                <#ident as waw::types::AudioModuleDescriptor>::parameter_descriptor_json()
+            }
         }
 
-        pub fn connect(&mut self) {
-          self.0.connect();
-        }
+        // Implement AudioModuleDescriptor for the provided module
+        impl waw::types::AudioModuleDescriptor for #ident {
+            fn processor_name() -> &'static str {
+                &#worklet_ident_name
+            }
 
-        pub fn process(
-          &mut self,
-          input: &waw::js_sys::Array,
-          output: &waw::js_sys::Array,
-          params: &wasm_bindgen::JsValue
-        ) -> bool {
-          self.0.process(input, output, params)
+            // Get the JSON descriptor for the parameters of the module
+            fn parameter_descriptor_json() -> String {
+                waw::serde_json::to_string(
+                    &<<#ident as waw::worklet::AudioModule>::Param as waw::types::ParameterDescriptor>::descriptors()
+                ).unwrap()
+            }
         }
-
-        pub fn parameter_descriptor() -> String {
-          <#ident as waw::types::AudioModuleDescriptor>::parameter_descriptor_json()
-        }
-      }
-
-      impl waw::types::AudioModuleDescriptor for #ident {
-        fn processor_name() -> &'static str {
-            &#worklet_ident_name
-        }
-
-        fn parameter_descriptor_json() -> String {
-          waw::serde_json::to_string(
-            &<<#ident as waw::worklet::AudioModule>::Param as waw::types::ParameterDescriptor>::descriptors()
-          ).unwrap()
-        }
-      }
     }
 }
